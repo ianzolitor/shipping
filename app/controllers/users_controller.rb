ca8cd273ba0b2250d90b8b6555b1e4ddd146af88
Ian Zolitor <ianzolitor@gmail.com>
2017-06-07 17:06:52 -0400

    signup creates new user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
 
   def create
     user=User.new(user_params)
-      if user.save
+      if user.save!
       session[:user_id] = user.id
     end
   end
@@ -22,5 +22,5 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:username, :password)
   end
-  
+
 end
