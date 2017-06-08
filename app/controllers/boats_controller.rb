56413bdeded3d862ed7f05dce61c022a662366a0
Ian Zolitor <ianzolitor@gmail.com>
2017-06-08 14:54:00 -0400

    create boat

--- app/controllers/boats_controller.rb
+++ app/controllers/boats_controller.rb
@@ -6,11 +6,21 @@ class BoatsController < ApplicationController
   end
 
   def create
+    boat=Boat.new(boat_params)
+      if boat.save!
+        render json: Boat.all
+    end
   end
 
   def update
   end
 
   def destroy
+  end
+
+  private
+  def boat_params
+    params.require(:boat).permit(:name,:container_amount,:location,:user_id)
+
   end
 end
